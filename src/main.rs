87aa2528306a3b092d9070868055c8a3f2955196
Macitron <117962989+Macitron@users.noreply.github.com>
2024-10-31 18:07:06 -0400

    Implement file extraction, single and all
    - ended up using a RefCell to the reader, which I initially felt very
      icky about (especially for my first Rust project) but eventually I
      found several people online who used the same pattern (including the
      tar-rs crate) so now I feel less bad

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,12 @@
-use std::fs::File;
-use std::io::BufReader;
-use std::path::Path;
 use ungelify::vfs::mpk::MpkArchive;
+use ungelify::vfs::Archive;
 
 fn main() {
     let filename: String = "resources/script.mpk".to_string();
-    // let filename: String = "resources/bad-ver.mpk".to_string();
-
-    let filepath = Path::new(&filename);
-
-    let mpk_file =
-        File::open(&filepath).expect(format!("could not open file '{}'", filename).as_str());
-    // eprintln!("opened file '{filename}': {mpk_file:?}");
-
-    let mut reader = BufReader::new(mpk_file);
-
-    let mut mpk = MpkArchive::from_mpk(&mut reader).unwrap();
-
-    eprintln!("loaded MPK archive: {mpk:#?}");
+    let mpk: MpkArchive = Archive::from_file(filename).unwrap();
 
     mpk.list_entries();
 
-    eprintln!("filepath parent is {:?}", filepath.parent().unwrap());
-    eprintln!("filepath stem is {:?}", filepath.file_stem().unwrap());
-
-    // SG03_03.SCX
-    mpk.extract_entry(filepath, "sg03_03.scx").unwrap();
+    mpk.extract_entry("sg03_03.scx").unwrap();
+    mpk.extract_all_entries().unwrap();
 }
--- src/vfs.rs
+++ src/vfs.rs
@@ -2,7 +2,18 @@ pub mod mpk;
 
 use std::error::Error;
 use std::io;
-use std::io::{Read, Seek, Write};
+use std::io::Read;
+use std::path::Path;
+
+pub trait Archive: Sized {
+    fn from_file<P: AsRef<Path>>(path: P) -> Result<Self, Box<dyn Error>>;
+
+    fn list_entries(&self);
+
+    fn extract_entry(&self, entry_name_or_id: &str) -> Result<(), Box<dyn Error>>;
+
+    fn extract_all_entries(&self) -> Result<(), Box<dyn Error>>;
+}
 
 fn read_signature(reader: &mut impl Read) -> Result<[u8; 4], io::Error> {
     let mut sig_buf = [0u8; 4];
@@ -10,7 +21,7 @@ fn read_signature(reader: &mut impl Read) -> Result<[u8; 4], io::Error> {
     Ok(sig_buf)
 }
 
-fn read_cstring<R: Read + Seek>(reader: &mut R) -> Result<String, Box<dyn Error>> {
+fn read_cstring(reader: &mut impl Read) -> Result<String, Box<dyn Error>> {
     let mut chars = Vec::new();
     let mut c = [0u8; 1];
     loop {
